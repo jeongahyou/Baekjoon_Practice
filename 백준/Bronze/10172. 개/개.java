a57917495d8cd42e8a847e86809814958b8fa7d5
jeongahyou <97575566+jeongahyou@users.noreply.github.com>
2023-03-27 03:26:34 +0900

    Update 개.java

--- 백준/Bronze/10172. 개/개.java
+++ 백준/Bronze/10172. 개/개.java
@@ -5,7 +5,8 @@ public static void main(String[] args) {
 	System.out.println("( 0 )\"\"\"\\");
 	System.out.println("|\"^\"`    |");
 	System.out.println("||_/=\\\\__|");
-
+// \를 쓸 땐 \\
+// "를 쓸 땐 \"
 }
 
 
