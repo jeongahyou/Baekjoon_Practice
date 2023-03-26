c2f32a49edb09c75b6dae69d220cf58159809eb7
jeongahyou <97575566+jeongahyou@users.noreply.github.com>
2023-03-27 03:14:12 +0900

    Update 꼬마 정민.java

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
-
+	
+	// int로 해서 런타임에러 떴음
 	long A = sc.nextLong();
 	long B = sc.nextLong();
 	long C = sc.nextLong();
